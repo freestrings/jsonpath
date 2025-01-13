7d20a294e847e6b065ad51fc06992ef2ad1cc1aa
freestrings <freestrings@gmail.com>
2025-01-13 23:24:34 +0900

    tests for ParserNodeVisitor

--- src/paths/parser_node_visitor.rs
+++ src/paths/parser_node_visitor.rs
@@ -13,66 +13,65 @@ pub trait ParserNodeVisitor<'a> {
         F1: Fn(&StrRange) -> &'a str,
     {
         trace!("visit {:?}", parse_node);
-        match &parse_node.token {
-            ParseToken::Absolute
-            | ParseToken::Relative
-            | ParseToken::All
-            | ParseToken::Key(_)
-            | ParseToken::Keys(_)
-            | ParseToken::Range(_, _, _)
-            | ParseToken::Union(_)
-            | ParseToken::Number(_)
-            | ParseToken::Bool(_) => {
-                token_handler.handle(&parse_node.token, parse_value_reader);
-            },
-            ParseToken::In | ParseToken::Leaves => {
-                if let Some(n) = &parse_node.left {
-                    self.visit(n, token_handler, parse_value_reader);
-                }
 
-                token_handler.handle(&parse_node.token, parse_value_reader);
+        // FIXME When written in "match" grammar, it is determined that "tarpaulin" did not cover the test coverage.
+        if &parse_node.token == &ParseToken::Absolute
+            || &parse_node.token == &ParseToken::Relative
+            || &parse_node.token == &ParseToken::All
+            || matches!(&parse_node.token, &ParseToken::Key(_))
+            || matches!(&parse_node.token, &ParseToken::Keys(_))
+            || matches!(&parse_node.token, &ParseToken::Range(_, _, _))
+            || matches!(&parse_node.token, &ParseToken::Union(_))
+            || matches!(&parse_node.token, &ParseToken::Number(_))
+            || matches!(&parse_node.token, &ParseToken::Bool(_))
+        {
+            token_handler.handle(&parse_node.token, parse_value_reader);
+        } else if &parse_node.token == &ParseToken::In
+            || &parse_node.token == &ParseToken::Leaves
+        {
+            if let Some(n) = &parse_node.left {
+                self.visit(n, token_handler, parse_value_reader);
+            }
 
-                if let Some(n) = &parse_node.right {
-                    self.visit(n, token_handler, parse_value_reader);
-                }
-            },
-            ParseToken::Array => {
-                if let Some(n) = &parse_node.left {
-                    self.visit(n, token_handler, parse_value_reader);
-                }
+            token_handler.handle(&parse_node.token, parse_value_reader);
 
-                token_handler.handle(&parse_node.token, parse_value_reader);
+            if let Some(n) = &parse_node.right {
+                self.visit(n, token_handler, parse_value_reader);
+            }
+        } else if &parse_node.token == &ParseToken::Array {
+            if let Some(n) = &parse_node.left {
+                self.visit(n, token_handler, parse_value_reader);
+            }
 
-                if let Some(n) = &parse_node.right {
-                    self.visit(n, token_handler, parse_value_reader);
-                }
+            token_handler.handle(&parse_node.token, parse_value_reader);
 
-                token_handler.handle(&ParseToken::ArrayEof, parse_value_reader);
-            },
-            ParseToken::Filter(FilterToken::And)
-            | ParseToken::Filter(FilterToken::Or) => {
-                if let Some(n) = &parse_node.left {
-                    self.visit(n, token_handler, parse_value_reader);
-                }
+            if let Some(n) = &parse_node.right {
+                self.visit(n, token_handler, parse_value_reader);
+            }
 
-                if let Some(n) = &parse_node.right {
-                    self.visit(n, token_handler, parse_value_reader);
-                }
+            token_handler.handle(&ParseToken::ArrayEof, parse_value_reader);
+        } else if &parse_node.token == &ParseToken::Filter(FilterToken::And)
+            || &parse_node.token == &ParseToken::Filter(FilterToken::Or)
+        {
+            if let Some(n) = &parse_node.left {
+                self.visit(n, token_handler, parse_value_reader);
+            }
 
-                token_handler.handle(&parse_node.token, parse_value_reader);
-            },
-            ParseToken::Filter(_) => {
-                if let Some(n) = &parse_node.left {
-                    self.visit(n, token_handler, parse_value_reader);
-                }
+            if let Some(n) = &parse_node.right {
+                self.visit(n, token_handler, parse_value_reader);
+            }
 
-                if let Some(n) = &parse_node.right {
-                    self.visit(n, token_handler, parse_value_reader);
-                }
+            token_handler.handle(&parse_node.token, parse_value_reader);
+        } else if matches!(&parse_node.token, &ParseToken::Filter(_)) {
+            if let Some(n) = &parse_node.left {
+                self.visit(n, token_handler, parse_value_reader);
+            }
 
-                token_handler.handle(&parse_node.token, parse_value_reader);
-            },
-            _ => {},
+            if let Some(n) = &parse_node.right {
+                self.visit(n, token_handler, parse_value_reader);
+            }
+
+            token_handler.handle(&parse_node.token, parse_value_reader);
         }
     }
 }
