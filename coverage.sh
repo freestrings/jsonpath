f4d620e92558b541f2818ef55d6fb412caf996d3
freestrings <freestrings@gmail.com>
2025-01-14 23:14:21 +0900

    Remove deprecated modules from coverage

--- coverage.sh
+++ coverage.sh
@@ -6,4 +6,12 @@
 
 set -e
 
-cargo tarpaulin --exclude-files nodejs wasm src/parser/mod.rs -v --all
+cargo tarpaulin --exclude-files \
+  nodejs \
+  wasm \
+  src/parser/mod.rs \
+  src/select/mod.rs \
+  src/select/cmp.rs \
+  src/select/expr_term.rs \
+  src/select/value_walker.rs \
+  -v --all
